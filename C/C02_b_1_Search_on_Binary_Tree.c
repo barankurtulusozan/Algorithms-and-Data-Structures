9d7b102c760fadb4c27b7056d4b2ee3965a3ba36
Baran Kurtuluş Ozan <barankozan@hotmail.com>
2015-03-20 23:51:18 +0200

    .

---
+++ C/C02_b_1_Search_on_Binary_Tree.c
@@ -0,0 +1,9 @@
+//C02_b_1_Search_on_Binary_Tree
+//Looking for a record on the Tree
+Tree *search(Tree *root, i nt searched_item){
+	while(root!=NULL && root->info!=searched_item)
+		if(searched_item<=searched_item->left);
+		else Tree=Tree->Right;
+	}
+	return Tree;
+}
